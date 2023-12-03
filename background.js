debe020316343d20283d96dae452fcd8773a0334
Diaa J <ditek@users.noreply.github.com>
2023-12-03 02:30:24 +0100

    Make sure each request is only redirected once

--- background.js
+++ background.js
@@ -1,22 +1,14 @@
-function isValidMatchPattern(pattern) {
-    try {
-        new URL(pattern.replace('*', 'test'));
-        return true;
-    } catch (e) {
-        return false;
-    }
-}
+let redirectedRequests = new Set();
 
 function redirect(requestDetails, suffix) {
-    let url = requestDetails.url;
-    if (!url.endsWith(suffix)) {
-        url += suffix;
-        console.log(`Redirecting ${requestDetails.url} to ${url}`);
-        return { redirectUrl: url };
-    } else {
-        console.log(`Not redirecting`);
+    if (redirectedRequests.has(requestDetails.requestId)) {
+        console.log(`Already redirected request ${requestDetails.requestId}`);
         return {};
     }
+    redirectedRequests.add(requestDetails.requestId);
+    let url = requestDetails.url + suffix;
+    console.log(`Redirecting ${requestDetails.url} to ${url}`);
+    return { redirectUrl: url };
 }
 
 browser.storage.local.get('patternsAndSuffixes').then(({ patternsAndSuffixes }) => {
@@ -24,17 +16,14 @@ browser.storage.local.get('patternsAndSuffixes').then(({ patternsAndSuffixes }) 
         return;
     }
     for (let { pattern, suffix } of patternsAndSuffixes) {
+        // Avoid "closure within loop" problem
         pattern = pattern;
         suffix = suffix;
         console.log(`Adding listener for ${pattern}`);
-        if (isValidMatchPattern(pattern)) {
-            browser.webRequest.onBeforeRequest.addListener(
-                (requestDetails) => redirect(requestDetails, suffix),
-                { urls: [pattern], types: ["main_frame"] },
-                ["blocking"]
-            );
-        } else {
-            console.log(`Invalid pattern: ${pattern}`);
-        }
+        browser.webRequest.onBeforeRequest.addListener(
+            (requestDetails) => redirect(requestDetails, suffix),
+            { urls: [pattern], types: ["main_frame"] },
+            ["blocking"]
+        );
     }
 });
