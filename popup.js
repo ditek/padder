61a0ea94b43f874d874aba881bc0b31ef05e98ed
BaraaJadaan <braajad1@gmail.com>
2023-12-10 23:53:45 +0300

    adding style to popup

--- popup.js
+++ popup.js
@@ -6,20 +6,23 @@ function isValidMatchPattern(pattern) {
         return false;
     }
 }
+let form = document.getElementById('form');
+let table = document.getElementById('table');
+let tbody = table.getElementsByTagName('tbody')[0];
+console.log(tbody);
 
 function updateTable() {
     // Get the existing patterns and suffixes
     browser.storage.local.get('patternsAndSuffixes').then(({ patternsAndSuffixes }) => {
+        // if patternsAndSuffixes is empty when opening the extention for the first time then hide the table
         if (!patternsAndSuffixes) {
-            patternsAndSuffixes = [];
+            table.style.display = 'none';
             return;
         }
-        let table = document.getElementById('table');
-        let tbody = table.getElementsByTagName('tbody')[0];
         // Clear the table
         tbody.innerHTML = '';
-        // Add each pattern and suffix to the table
-        for (let i = 0; i < patternsAndSuffixes.length; i++) {
+        // Add each pattern and suffix to the table in reverse order
+        for (let i = patternsAndSuffixes.length - 1; i >= 0; i--) {
             let item = patternsAndSuffixes[i];
             let row = tbody.insertRow();
             let patternCell = row.insertCell();
@@ -29,6 +32,8 @@ function updateTable() {
             let actionCell = row.insertCell();
             let deleteButton = document.createElement('button');
             deleteButton.textContent = 'Delete';
+            // // Add the animate class to the new row
+            // tbody.rows[patternsAndSuffixes.length - 1 - i].classList.add('animate')
             deleteButton.addEventListener('click', function () {
                 // Delete the pattern and suffix and update
                 patternsAndSuffixes.splice(i, 1);
@@ -37,6 +42,13 @@ function updateTable() {
             });
             actionCell.appendChild(deleteButton);
         }
+        // Check if tbody is empty and hide the table if it is(because table head stays when deleting all rows manually)
+        if (!tbody.hasChildNodes()) {
+            table.style.display = 'none';
+        } else {
+            table.style.display = '';
+        }
+
     });
 }
 
@@ -48,6 +60,9 @@ document.getElementById('form').addEventListener('submit', function (event) {
         console.error(`Invalid pattern: ${pattern}`);
         document.getElementById('error').textContent = 'Invalid pattern';
         return;
+    }
+    else{
+        document.getElementById('error').textContent = ''
     }
     // Add to existing patterns and suffixes
     browser.storage.local.get('patternsAndSuffixes').then(({ patternsAndSuffixes }) => {
@@ -64,6 +79,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
         browser.storage.local.set({ patternsAndSuffixes });
         updateTable();
     });
+    form.reset();
 });
 
 // Update the list when the popup is opened
